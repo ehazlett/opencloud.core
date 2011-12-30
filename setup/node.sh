aa3f2fccf0700a279974a97ad82cafc9b274cf7c
Evan Hazlett <ejhazlett@gmail.com>
2011-12-30 08:29:20 -1000

    updated node setup

--- setup/node.sh
+++ setup/node.sh
@@ -12,34 +12,36 @@ if [ "$(id -u)" != "0" ]; then echo "Error: You must be root to run setup"; exit
 
 if [ -z $1 ] ; then show_help ; fi
 
-## install dependencies
-#apt-get update && apt-get -y upgrade
-#apt-get -y install build-essential irb libopenssl-ruby libreadline-ruby rdoc ri ruby ruby-dev rubygems supervisor
-#
-## install facter
-#wget http://downloads.puppetlabs.com/facter/facter-1.6.1.tar.gz
-#tar zxf facter*
-#cd facter* ; sudo ruby install.rb
-#cd ../ ; rm -rf facter*
-#
-## install puppet
-#wget http://puppetlabs.com/downloads/puppet/puppet-latest.tgz
-#tar zxf puppet-latest.tgz
-#cd puppet* ; sudo ruby install.rb
-#cd ../ ; rm -rf puppet*
-#
-## create puppet group
-#groupadd puppet
-#
-## create supervisor config for puppet master
-#echo "[program:puppet-agent]
-#command=/usr/bin/puppet agent
-#  --verbose
-#  --no-daemonize
-#user=root
-#stopsignal=QUIT" > /etc/supervisor/conf.d/puppet-agent.conf
-#
-#supervisorctl update
+# install dependencies
+apt-get update && apt-get -y upgrade
+apt-get -y install build-essential irb libopenssl-ruby libreadline-ruby rdoc ri ruby ruby-dev rubygems supervisor
+
+# install facter
+wget http://downloads.puppetlabs.com/facter/facter-1.6.1.tar.gz
+tar zxf facter*
+cd facter* ; sudo ruby install.rb
+cd ../ ; rm -rf facter*
+
+# install puppet
+wget http://puppetlabs.com/downloads/puppet/puppet-latest.tgz
+tar zxf puppet-latest.tgz
+cd puppet* ; sudo ruby install.rb
+cd ../ ; rm -rf puppet*
+
+# create puppet group
+groupadd puppet
+
+# create supervisor config for puppet master
+echo "[program:puppet-agent]
+command=/usr/bin/puppet agent
+  --verbose
+  --no-daemonize
+user=root
+stopsignal=QUIT" > /etc/supervisor/conf.d/puppet-agent.conf
+
+supervisorctl update
+
+# setup puppet host
 PUPPET_HOST=`grep puppet /etc/hosts`
 if [ -n "$PUPPET_HOST" ]; then
   echo "Warning: Puppet host previously setup.  Remove the entry from /etc/hosts for setup to configure."
