80e4d5f77a59fcd1973833990b4b20aead566b9e
Evan Hazlett <ejhazlett@gmail.com>
2012-01-01 20:08:08 -1000

    added snmp config

--- puppet/modules/common/manifests/init.pp
+++ puppet/modules/common/manifests/init.pp
@@ -1,5 +1,8 @@
 class common {
   $opencloud_conf_dir = "/etc/opencloud"
+  $snmp_rw_community = "opencloud"
+  $snmp_sys_location = "Default Location"
+  $snmp_sys_contact = "Default Contact <root@localhost>"
 
   define base_packages() {
     package { "${name}":
@@ -14,6 +17,7 @@ class common {
     "python-dev",
     "python-setuptools",
     "snmpd",
+    "snmp-mibs-downloader",
     "supervisor",
     "zip",
   ]: }
@@ -22,11 +26,24 @@ class common {
     path    => "${::path}",
   }
 
+  service { "snmpd":
+    ensure    => running,
+  }
+
   file { "common::opencloud_conf_dir":
     ensure  => directory,
     path    => "${common::opencloud_conf_dir}",
     owner   => root,
     group   => root,
+  }
+
+  file { "common::snmp_conf":
+    path    => "/etc/snmp/snmpd.conf",
+    content => template("common/snmpd.conf.erb"),
+    owner   => root,
+    group   => root,
+    require => Package["snmpd"],
+    notify  => Service["snmpd"],
   }
 
 }
